a82b2a63568368918dd60e2216d726ac82a8983e
gatraenggar <gatraenggarwicaksono@gmail.com>
2021-10-19 11:27:51 +0800

    implement django-cors-headers

--- server/settings.py
+++ server/settings.py
@@ -18,12 +18,39 @@ INSTALLED_APPS = [
     'auth',
     'users',
     'workspaces',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    config('CLIENT'),
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 ROOT_URLCONF = 'server.urls'
